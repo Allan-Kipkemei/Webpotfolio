8133754a5044ca2bb6b3e33f51d6219b8dd9cff9
Allan Kiplagat <allankiplgat@gmail.com>
2025-12-24 18:37:50 +0300

    Blogs prep.....

--- src/app/components/BlogList.tsx
+++ src/app/components/BlogList.tsx
@@ -1,11 +1,25 @@
 import BlogListItem from "./BlogListItem";
 import { TPost } from "../utils/types";
 
-export default function BlogList({ blogs, isHome }: { blogs: TPost[], isHome?: boolean }) {
+export default function BlogList({
+    blogs,
+    isHome,
+}: {
+    blogs: TPost[];
+    isHome?: boolean;
+}) {
+    if (blogs.length === 0) {
+        return (
+            <div className="w-full text-center py-10 text-gray-500 text-lg">
+                🚧 Blogs coming soon!
+            </div>
+        );
+    }
+
     return (
-        <div className={`flex flex-col blog-list w-full ${!isHome && "gap-2"}`}>
+        <div className={`flex flex-col blog-list w-full ${!isHome ? "gap-2" : ""}`}>
             {blogs.map((blog) => (
-                <BlogListItem key={blog.id} blog={blog} isHome={isHome}/>
+                <BlogListItem key={blog.id} blog={blog} isHome={isHome} />
             ))}
         </div>
     );
